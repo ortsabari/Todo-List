3278b35aff6ec79ff00b6562c1d444bfcba7616b
or tsabari <ortsabari@ors-MacBook-Pro.local>
2025-11-22 21:50:41 +0200

    remove feature plus css and id

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // Task Class
 class Task {
+  id;
   description;
   date;
   time;
 
-  constructor(description, date, time) {
+  constructor(id, description, date, time) {
+    this.id = id;
     this.description = description;
     this.date = date;
     this.time = time;
@@ -24,22 +26,27 @@ function newTask(event) {
   const description = form.description.value;
   const date = form.date.value;
   const time = form.time.value;
-
-  const taskObj = new Task(description, date, time);
+  const id = taskList.length == 0 ? 1 : taskList[taskList.length - 1].id + 1;
+  const taskObj = new Task(id, description, date, time);
   taskList.push(taskObj);
   localStorage.setItem("tasklist", JSON.stringify(taskList));
   form.reset();
 
   generateNewNote();
 }
-
+console.log(taskList);
 // function to render array from localstorage and convert to class
 function renderArray() {
   const taskList = [];
   const localGet = JSON.parse(localStorage.getItem("tasklist")) || [];
   for (let i = 0; i < localGet.length; i++) {
     const local = localGet[i];
-    const taskNewObj = new Task(local.description, local.date, local.time);
+    const taskNewObj = new Task(
+      local.id,
+      local.description,
+      local.date,
+      local.time
+    );
     taskList.push(taskNewObj);
   }
   return taskList || [];
@@ -57,7 +64,7 @@ function generateNewNote() {
 
     const noteFirstP = document.createElement("p");
     noteFirstP.innerText = list.description;
-    noteFirstP.setAttribute("class", "descriptionpara");
+    noteFirstP.setAttribute("class", "content_para");
 
     const noteSecondP = document.createElement("p");
     noteSecondP.innerText = list.date;
@@ -67,7 +74,21 @@ function generateNewNote() {
     noteThirdP.innerText = list.time;
     noteThirdP.setAttribute("class", "timepara");
 
-    contentContainer.append(noteFirstP, noteSecondP, noteThirdP);
+    let x = document.createElement("img");
+    x.setAttribute("src", "images/cancel.png");
+    x.setAttribute("class", "cancel");
+    x.setAttribute("onclick", "removeNote(" + list.id + ")");
+
+    contentContainer.append(x, noteFirstP, noteSecondP, noteThirdP);
     notes.append(contentContainer);
   }
 }
+
+function removeNote(listid) {
+  const index = taskList.findIndex((item) => item.id == listid);
+  taskList.splice(index, 1);
+  localStorage.setItem("tasklist", JSON.stringify(taskList));
+  generateNewNote(); // update the list of the cards
+}
+
+// in order to find last id in the array you need to check arr length - 1
